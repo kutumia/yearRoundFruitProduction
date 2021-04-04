618a00a259ae860d60c67d60aef05ac0fde87504
kutumia <rhythmrahman007@gmail.com>
2021-04-04 11:09:30 +0600

    server

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config()
+const { DBUSER, DBPASSWORD, DB } = process.env;
+//console.log(DBUSER, DBPASSWORD)
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "yearroundfruit",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "localhost",
+  USER: DBUSER,
+  PASSWORD: DBPASSWORD,
+  DB: DB,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
