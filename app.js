1a4ec44fcb7c7dafc92b3eb9c4200639fb5ab86e
njayman <najishm828282@gmail.com>
2021-04-04 11:37:03 +0600

    deployment fixes

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var localStorage=require('local-storage');
-var dotenv= require('dotenv');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var localStorage = require("local-storage");
+var dotenv = require("dotenv");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var centerRouter = require('./routes/center');
-var pdRouter = require('./routes/pd');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var centerRouter = require("./routes/center");
+var pdRouter = require("./routes/pd");
 
-dotenv.config({path:'./.env'});
+dotenv.config({ path: "./.env" });
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: Date.now() + (30 * 86400 * 1000)  }}))
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: "keyboard cat",
+    cookie: { maxAge: Date.now() + 30 * 86400 * 1000 },
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.locals.type = req.session.type;
-  console.log("app.js",req.session.type,res.locals.type)
+  console.log("app.js", req.session.type, res.locals.type);
   res.locals.user_id = req.session.user_id;
   next();
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/center', centerRouter);
-app.use('/pd', pdRouter);
-
-
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/center", centerRouter);
+app.use("/pd", pdRouter);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -56,13 +57,13 @@ app.use('/pd', pdRouter);
 //   res.render('error');
 // });
 
-
-
 const db = require("./models");
-db.sequelize.sync();
-
-db.sequelize.sync({ force: false }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+db.sequelize.sync().catch((err) => console.log(err.message));
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Drop and re-sync db.");
+  })
+  .catch((err) => console.log(err.message));
 
 module.exports = app;
--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,14 @@
+require("dotenv").config();
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "yearroundfruit",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "localhost",
+  USER: process.env.UNAME,
+  PASSWORD: process.env.PASSWORD,
+  DB: "yearroundfruit",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
